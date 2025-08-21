cf87c7246724961aa049c7fc6e753204cdf65865
Risto Autio <risto.autio@iodigital.com>
2025-08-21 11:57:35 +0200

    test extensions

--- src/main/java/com/example/springcloudcontractdemo/TestController.java
+++ src/main/java/com/example/springcloudcontractdemo/TestController.java
@@ -1,13 +1,16 @@
 package com.example.springcloudcontractdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
     @GetMapping("/test")
     public String test() {
+        log.info("Request received at /test endpoint");
         return "Test successful!";
     }
 }
---
+++ src/test/java/com/example/springcloudcontractdemo/TestControllerIT.java
@@ -0,0 +1,36 @@
+package com.example.springcloudcontractdemo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@ExtendWith(OutputCaptureExtension.class)
+public class TestControllerIT {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void testEndpointReturnsSuccessMessage(CapturedOutput capturedOutput) {
+        webTestClient
+            .get()
+            .uri("/test")
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectBody(String.class)
+            .isEqualTo("Test successful!");
+
+        assertThat(capturedOutput.getOut())
+            .contains("Request received at /test endpoint");
+    }
+}
